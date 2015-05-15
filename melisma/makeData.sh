ac88eba62e3dea0bb38f7d23d315f1076d34cd6c
John Pope <jp@bellgeorge.com>
2015-05-15 11:38:16 +1000

    critical fix for mftext returning no notes.

--- melisma/makeData.sh
+++ melisma/makeData.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-#!/usr/bin/make -f
-for file in mftext/midi/*
+
+for file in mftext/midi/*.mid
 do
     mftext/mftext "$file" | meter/meter | harmony/harmony | key/key | tr -d '\n' > "$file".txt
 done
